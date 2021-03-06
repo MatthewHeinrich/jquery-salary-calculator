5c35378594367dc8ec464cae347ab9f6b18c31b1
MatthewHeinrich <mheinrich95@gmail.com>
2021-03-06 10:17:49 -0600

    added delete button and styling

--- scripts/client.js
+++ scripts/client.js
@@ -1,11 +1,15 @@
 $( document ).ready(onReady);
 
 let employees = [];
-let wage = 1;
+// let newWage = 0;
 
 function onReady() {
     $('#submitButton').on('click', addEmployee);
     $('#sumbitButton').on('click', showEmployees);
+    $("#employeeOut").on('click', '#delete', function() {
+        $(this).closest('tr').remove();
+    });
+   
 } // end onReady
 
 
@@ -26,6 +30,7 @@ function addEmployee() {
     $('#annuaSalaryIn').val('');
 
     showEmployees();
+
     
    
     
@@ -35,13 +40,36 @@ function addEmployee() {
 function showEmployees() {
     let el = $('#employeeOut');
     let count = $('#totaMonthly');
+    let wage = 0;
     el.empty();
     count.empty();
     for (i=0; i<employees.length; i++) {
-        el.append(`<tr><td>${employees[i].first}</td> <td> ${employees[i].last}</td> <td> ${employees[i].id}</td> <td> ${employees[i].title}</td> <td> ${employees[i].salary}</td></tr>`);
-        wage+=Number(employees[i].salary);
-        count.append(wage);
+         wage+=Number(employees[i].salary);
+         if(wage > 20000) {
+            var style = document.createElement('style');
+                style.innerHTML = `
+                #monthlyOut {
+                background-color: red;
+                }
+                `;
+                 document.head.appendChild(style);
+
+         }
+        // count.append(wage);
+        el.append(`<tr><td>${employees[i].first}</td> <td class="tableIn"> ${employees[i].last}</td> <td class="tableIn"> ${employees[i].id}</td> <td class="tableIn"> ${employees[i].title}</td> <td class="tableIn"> ${employees[i].salary}</td><td class="tableIn"><button id="delete">Delete</button></td></tr>`);
+        // wage = wage + Number(employees[i].salary);
+        // count.append(wage);
     } // end for loop
+    count.append(wage);
+    // newWage.push(wage);
+    
 } // end showEmployees
 
-
+// function addEmployeeWage() {
+//     let count = $('#totaMonthly');
+//     count.empty();
+//     for (salary of employeess.salary) {
+//         wage = wage + Number(employees.salary);
+//         count.append(wage);
+//     }
+// }
